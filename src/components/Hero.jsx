ee4049d49062af81d78ed54227f5eae2bb943738
Anubrata Das <anubrata.dev4space@gmail.com>
2026-03-04 21:52:46 +0530

    Changed hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,24 @@
+import heroBg from "../assets/hero-bg.jpg";
+
 export default function Hero() {
   return (
-    <>
-      <style>
-        {`
-          #hero {
-            background-image: url('../images/hero-bg.jpg');
-            padding: 80px 8%;
-            text-align: center;
-          }
-        
-        `}
-      </style>
-      <section id="hero" className="hero">
-        <h1>AI-Driven Preventive Healthcare System</h1>
-        <p>
-         <i>National Level Innovation | AIR 27</i>
-        </p><br></br>
-        <button className="btn">Explore Architecture</button>
-      </section>
-    </>
+    <section
+      id="hero"
+      className="hero"
+      style={{
+        backgroundImage: `url(${heroBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        padding: "80px 8%",
+        textAlign: "center",
+      }}
+    >
+      <h1>AI-Driven Preventive Healthcare System</h1>
+      <p>
+        <i>National Level Innovation | AIR 27</i>
+      </p>
+      <br />
+      <button className="btn">Explore Architecture</button>
+    </section>
   );
 }
